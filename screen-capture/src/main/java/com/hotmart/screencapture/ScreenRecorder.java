8baacc72106d6ecd90752690a36601335c680da1
Luiz Bueno <luizbueno@hotmart.com>
2018-05-15 20:22:53 -0300

    Nova versão

--- screen-capture/src/main/java/com/hotmart/screencapture/ScreenRecorder.java
+++ screen-capture/src/main/java/com/hotmart/screencapture/ScreenRecorder.java
@@ -7,6 +7,7 @@ import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Vector;
 import java.util.concurrent.TimeUnit;
@@ -21,6 +22,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.apache.commons.io.comparator.NameFileComparator;
 
 /**
  * Main class that starts the Recording process of EasyCapture.
@@ -67,11 +70,8 @@ public class ScreenRecorder {
 				try {
 					rt = new Robot();
 					while (cnt == 0 || record) {
-						BufferedImage img = rt
-								.createScreenCapture(new Rectangle(screenWidth,
-										screenHeight));
-						ImageIO.write(img, "jpeg", new File("./"+store+"/"
-								+ System.currentTimeMillis() + ".jpeg"));
+						BufferedImage img = rt.createScreenCapture(new Rectangle(screenWidth,screenHeight));
+						ImageIO.write(img, "jpeg", new File("./"+store+"/"+ System.currentTimeMillis() + ".jpeg"));
 						if (cnt == 0) {
 							record = true;
 							cnt = 1;
@@ -87,29 +87,30 @@ public class ScreenRecorder {
 		recordThread.start();
 	}
 
-//	/**
-//	 * @throws MalformedURLException 
-//	 * 
-//	 */
-//	public static void makeVideo(String movFile) throws MalformedURLException {
-//		System.out.println("#### Easy Capture making video, please wait!!! ####");
-//		JpegImagesToMovie imageToMovie = new JpegImagesToMovie();
-//		Vector<String> imgLst = new Vector<String>();
-//		File f = new File(store);
-//		File[] fileLst = f.listFiles();
-//		for (int i = 0; i < fileLst.length; i++) {
-//			imgLst.add(fileLst[i].getAbsolutePath());
-//		}
-//		// Generate the output media locators.
-//		MediaLocator oml;
-//		if ((oml = imageToMovie.createMediaLocator(movFile)) == null) {
-//			System.err.println("Cannot build media locator from: " + movFile);
-//			System.exit(0);
-//		}
-//		imageToMovie.doIt(screenWidth, screenHeight, (1000 / captureInterval),
-//				imgLst, oml);
-//
-//	}
+	/**
+	 * @throws MalformedURLException 
+	 * 
+	 */
+	public static void makeVideo(String movFile) throws MalformedURLException {
+		System.out.println("#### Easy Capture making video, please wait!!! ####");
+		JpegImagesToMovie imageToMovie = new JpegImagesToMovie();
+		Vector<String> imgLst = new Vector<String>();
+		File f = new File(store);
+		File[] fileLst = f.listFiles();
+		Arrays.sort(fileLst, NameFileComparator.NAME_COMPARATOR);
+		for (int i = 0; i < fileLst.length; i++) {
+			imgLst.add(fileLst[i].getAbsolutePath());
+		}
+		// Generate the output media locators.
+		MediaLocator oml;
+		if ((oml = imageToMovie.createMediaLocator(movFile)) == null) {
+			System.err.println("Cannot build media locator from: " + movFile);
+			System.exit(0);
+		}
+		imageToMovie.doIt(screenWidth, screenHeight, (1000 / captureInterval),
+				imgLst, oml);
+
+	}
 
 	/**
 	 * @param args
@@ -119,63 +120,39 @@ public class ScreenRecorder {
 		System.out.println("######### Starting Easy Capture Recorder #######");
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		System.out.println("Your Screen [Width,Height]:" + "["+ screen.getWidth() + "," + screen.getHeight() + "]");
-		//Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Rate 20 Frames/Per Sec.");
+		System.out.print("Do you wanna change the screen capture area (y/n) ? ");
+		
+		if (sc.next().equalsIgnoreCase("y")) {
+			System.out.print("Enter the width:");
+			screenWidth = sc.nextInt();
+			System.out.print("Enter the Height:");
+			screenHeight = sc.nextInt();
+			System.out.println("Your Screen [Width,Height]:" + "["+ screen.getWidth() + "," + screen.getHeight() + "]");
+		}
 		
 		System.out.print("Now move to the screen you want to record");
-		
-	    //Create a new instance of the Firefox driver
-	    WebDriver driver = new FirefoxDriver();
-	    driver.manage().window().fullscreen();
-	 
-	    //Call the start method of ScreenRecorder to begin recording
+		for(int i=0;i<5;i++){
+			System.out.print(".");
+			Thread.sleep(1000);
+		}
 		File f = new File(store);
 		if(!f.exists()){
 			f.mkdir();
 		}
 		startRecord();
 		System.out.println("\nEasy Capture is recording now!!!!!!!");
-	 
-		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
-		driver.get("http://www.hotmart.com/pt/"); 
 
-		//Create an action object called myMouse
-		Actions myMouse = new Actions(driver); 
-
-		WebElement entreLink = driver.findElement(By.linkText("Entre")); 
-		myMouse.moveToElement(entreLink).build().perform();   //Shows link
-		Thread.sleep(5000L);
-		entreLink.click();
-		
-		Thread.sleep(5000L);
-		
-		WebElement digiteSeuUsuarioLabel = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div/div/div/div/div/div[2]/div[1]/div[2]/form[2]/div/span[1]/input"));
-		myMouse.moveToElement(digiteSeuUsuarioLabel).build().perform();   //home link
-		Thread.sleep(5000L);
-		digiteSeuUsuarioLabel.sendKeys("luiz.bueno@hotmart.com");
-		
-		Thread.sleep(5000L);
-		
-		WebElement senhaLink = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div/div/div/div/div/div[2]/div[1]/div[2]/form[2]/div/span[2]/input")); 
-		myMouse.moveToElement(senhaLink).build().perform();
-		Thread.sleep(5000L);
-		
-		Thread.sleep(5000L);
-		
-		WebElement botaoEntrar = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div/div/div/div/div/div[2]/div[1]/div[2]/form[2]/button")); 
-		myMouse.moveToElement(botaoEntrar).build().perform();
-		Thread.sleep(5000L);
-		botaoEntrar.click();
-		
-		Thread.sleep(5000L);
- 
-	    //Close the browser
-	    driver.quit();
-	 
-	    //Call the stop method of ScreenRecorder to end the recording
+		System.out.println("Press e to exit:");
+		String exit = sc.next();
+		while (exit == null || "".equals(exit) || !"e".equalsIgnoreCase(exit)) {
+			System.out.println("\nPress e to exit:");
+			exit = sc.next();
+		}
 		record = false;
 		System.out.println("Easy Capture has stopped.");
-		//makeVideo(System.currentTimeMillis()+".mov");
+		makeVideo(System.currentTimeMillis()+".mov");
 	}
 }
 
